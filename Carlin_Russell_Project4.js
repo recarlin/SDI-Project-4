3742852880928f956636f417efe639b20b3b5ea0
Russell Carlin <recarlin@fullsail.com>
2011-12-14 20:38:48 -0500

    Working on tests
    Took me forever to find a good site to look up regex metacharacters and
    such, but I found one and now I'm just building the tests.

--- Carlin_Russell_Project4.js
+++ Carlin_Russell_Project4.js
@@ -1,1 +1,18 @@
-alert("JavaScript works!");
+//Russell Carlin
+//SDI 1112
+//Project 4
+var mahLib = function(v) {
+    var tests = {
+        testPhoneNumber: function(n) {
+            var p = /^(\d{3})[- ]?(\d{3})[- ]?(\d{4})$/;  
+            return p.test(n);  
+        },
+        testEmail: function(n) {
+            var p = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+            return p.test(n)
+        },
+        testURL: function(n) {
+            var p = /^(http:|https:)/
+        }
+    };
+};
