5d480ad2b1588b7729d231545082dd454fb179a2
Russell Carlin <recarlin@fullsail.com>
2011-12-14 21:38:52 -0500

    tCase function
    Done.

--- Carlin_Russell_Project4.js
+++ Carlin_Russell_Project4.js
@@ -1,18 +1,26 @@
 //Russell Carlin
 //SDI 1112
 //Project 4
-var mahLib = function(v) {
-    var tests = {
-        testPhoneNumber: function(n) {
-            var p = /^(\d{3})[- ]?(\d{3})[- ]?(\d{4})$/;  
-            return p.test(n);  
-        },
-        testEmail: function(n) {
-            var p = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-            return p.test(n)
-        },
-        testURL: function(n) {
-            var p = /^(http:|https:)/
-        }
-    };
+//var mahLib = function(v) {   
+var tests = {
+    phoneNumber: function(n) {
+        var p = /^(\d{3})-(\d{3})-(\d{4})$/;  
+        return p.test(n);  
+    },
+    email: function(n) {
+        var p = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        return p.test(n)
+    },
+    URL: function(n) {
+        var p = /^(http:|https:)/
+        return p.test(n)
+    },
+    tCase: function(n) {
+        var s = n.split(" ")
+        for (i=0, l=s.length;i<l;i++) {
+            s[i] = s[i].slice(0,1).toUpperCase() + s[i].slice(1);
+        };
+        return s.join(" ")
+    }
 };
+console.log(tests.tCase("Are you watching?"))
